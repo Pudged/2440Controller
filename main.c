116fe21f610869cedbe84212dd88c5e32544b242
Ivan Rodriguez <brro6217@colorado.edu>
2020-04-29 11:39:55 -0600

    Buttons Done

--- main.c
+++ main.c
@@ -1,47 +1,4 @@
-/**
- * Copyright (c) 2015 - 2019, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/**
- * @brief Blinky Sample Application main file.
- *
- * This file contains the source code for a sample server application using the LED Button service.
- */
+// Bluetooth Retro Game Controller
 
 #include <stdint.h>
 #include <string.h>
@@ -64,7 +21,7 @@
 #include "nrf_ble_qwr.h"
 #include "nrf_pwr_mgmt.h"
 #include "nrf_drv_gpiote.h"
-
+//#include "app_uart.h"
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 #include "nrf_log_default_backends.h"
@@ -104,6 +61,8 @@
 
 #define DEAD_BEEF                       0xDEADBEEF                              /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
 
+#define UART_TX_BUF_SIZE                256
+#define UART_RX_BUF_SIZE                256  
 
 BLE_LBS_DEF(m_lbs);                                                             /**< LED Button Service instance. */
 NRF_BLE_GATT_DEF(m_gatt);                                                       /**< GATT module instance. */
@@ -493,6 +452,7 @@ static void ble_stack_init(void)
  * @param[in] pin_no        The pin that the event applies to.
  * @param[in] button_action The button action (press/release).
  */
+// button IRQ essentially
 static void button_event_handler(uint8_t pin_no, uint8_t button_action)
 {
     ret_code_t err_code;
@@ -564,6 +524,7 @@ static void button_event_handler(uint8_t pin_no, uint8_t button_action)
             {
                 APP_ERROR_CHECK(err_code);
             }
+            bsp_board_led_invert(LEDBUTTON_LED); // to make LED turn on and off during pause press
             break;
         
         case RIGHT_TRIGGER:
@@ -597,11 +558,15 @@ static void button_event_handler(uint8_t pin_no, uint8_t button_action)
 
 /**@brief Function for initializing the button handler module.
  */
+// initially had error with sdk_congig only being setup to 4 low power events #define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS 7
+//                                                                            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
 static void buttons_init(void)
 {
     ret_code_t err_code;
 
     //The array must be static because a pointer to it will be saved in the button handler module.
+    // using the button variable instead of GPIOTE allowed for easier implementation. button library already uses GPIOTE.
     static app_button_cfg_t buttons[] =
     {
         //{LEDBUTTON_BUTTON, false, BUTTON_PULL, button_event_handler},
@@ -612,7 +577,7 @@ static void buttons_init(void)
         {PAUSE_BUTTON, true, NRF_GPIO_PIN_NOPULL, button_event_handler},
         {LEFT_TRIGGER, true, NRF_GPIO_PIN_NOPULL, button_event_handler},
         {RIGHT_TRIGGER, true, NRF_GPIO_PIN_NOPULL, button_event_handler}
-    };
+    }; // button array for 5 face buttons and side triggers
 
     /*static app_button_cfg_t buttons2[] =
     {
@@ -623,7 +588,7 @@ static void buttons_init(void)
 
     err_code = app_button_init(buttons, ARRAY_SIZE(buttons), BUTTON_DETECTION_DELAY);
     char const *error = nrf_strerror_get(err_code);
-    printf("%s\n", error);
+    printf("%s\n", error); // debugging purposes
     APP_ERROR_CHECK(err_code);
 
     //err_code = app_button_init(buttons2, 3, BUTTON_DETECTION_DELAY);
@@ -662,6 +627,11 @@ static void idle_state_handle(void)
     }
 }
 
+
+/*
+    This was going to be my original interrupt handler for the buttons, but then I decided to use the button event
+    handler from the example,
+*/
 /*void in_pin_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action)
 {
     switch(pin)
@@ -669,15 +639,27 @@ static void idle_state_handle(void)
         case A_BUTTON:
             nrf_drv_gpiote_out_toggle(7);
             break;
+        case B_BUTTON:
+            nrf_drv_gpiote_out_toggle(22);
+            break;
+        case X_BUTTON:
+            nrf_drv_gpiote_out_toggle(21);
+            break;
+        case Y_BUTTON:
+            nrf_drv_gpiote_out_toggle(23);
+            break;
+        case PAUSE_BUTTON:
+            nrf_drv_gpiote_out_toggle(9);
+            break;
     }
 }*/
 
 /**
- * setup button pins as GPIO inputs
+ * setup button pins as GPIO inputs, one of my original setups, but it was better to initialize them as GPIOTE or buttons.
  * 
  */
 
-/*static void gpio_init(void)
+/*void gpio_init(void)
 {
     nrf_gpio_cfg_input(A_BUTTON, NRF_GPIO_PIN_NOPULL);
     nrf_gpio_cfg_input(B_BUTTON, NRF_GPIO_PIN_NOPULL);
